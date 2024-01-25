6dacffbdcde1e34ce8e0998fcf520c89a041250c
sudiptodas153 <sudiptodas4093@gmail.com>
2024-01-25 11:35:50 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
+
+  // < ul >
+                  // <li><a href="https://www.facebook.com/profile.php?id=100094053365815"></a>Facebook</li>
+                  // <li><a href=""></a>Whatsapp</li>
+                  // <li><a href="https://www.tiktok.com/@as.sudipto153?is_from_webapp=1&sender_device=pc"></a>Tiktok</li>
+                // </ul >
